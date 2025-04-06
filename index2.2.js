cd2b778ed1c768b64982d55cdd76412cd6711a4b
ГУЛЬФИЯ <Gulepha@mail.ru>
2025-04-06 22:40:59 +0500

    Add files via upload

---
+++ index2.2.js
@@ -0,0 +1,37 @@
+let a = parseInt(prompt("введите от 1 до 9:"));
+if (a===1)
+{
+	alert("!")
+}
+else if (a===2)
+{
+	alert("@")
+}
+else if (a===3)
+{
+	alert("#")
+}
+else if (a===4)
+{
+	alert("$")
+}
+else if (a===5)
+{
+	alert("%")
+}
+else if (a===6)
+{
+	alert("^")
+}
+else if (a===7)
+{
+	alert("&")
+}
+else if (a===8)
+{
+	alert("*")
+}
+else if (a===9)
+{
+	alert("*")
+}
